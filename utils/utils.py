8a59ea7cb3cedfef5c1900b6d3127b91d414199b
chris levy <christopher@dashhudson.com>
2023-01-15 14:21:15 -0400

    wips

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,8 @@
 import torch
 from torch.utils.data import WeightedRandomSampler
+from tensorflow import keras
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def square_distance(Xb, Xq):
@@ -32,3 +35,29 @@ def kmeans(data, k, iterations=10):
         for j in range(k):  # TODO how to do without a for loop
             centroids[j] = data[assigned_clusters == j].mean(dim=0)
     return centroids, assigned_clusters
+
+
+class LRFinderCallback(keras.callbacks.Callback):
+    def __init__(self, lr_factor=1.1, max_lr=1e-1):
+        super().__init__()
+        self.factor = lr_factor
+        self.lrs = []
+        self.losses = []
+        self.max_lr = max_lr
+
+    def get_lr(self):
+        return float(keras.backend.get_value(self.model.optimizer.learning_rate))
+
+    def set_lr(self, lr):
+        keras.backend.set_value(self.model.optimizer.lr, lr)
+
+    def on_train_batch_end(self, batch, logs=None):
+        lr = self.get_lr()
+        self.lrs.append(lr)
+        self.losses.append(logs["loss"])
+
+        if lr > self.max_lr:
+            self.model.stop_training = True
+            plt.plot(np.log10(self.lrs), self.losses)
+
+        self.set_lr(lr * self.factor)
